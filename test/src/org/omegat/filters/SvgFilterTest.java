d22581d5022d37b2acfd90f8e1fcb5d901ef9399
alex73 <alex73@b0d8beef-cb45-0410-a8e4-c0d495c3b779>
2011-03-16 14:36:22 +0000

    SVG filter's test data changed

--- test/src/org/omegat/filters/SvgFilterTest.java
+++ test/src/org/omegat/filters/SvgFilterTest.java
@@ -4,17 +4,17 @@ import org.omegat.core.data.IProject;
 import org.omegat.filters3.xml.svg.SvgFilter;
 
 public class SvgFilterTest extends TestFilterBase {
-   
+
     public void testLoad() throws Exception {
-        String f = "test/data/filters/SVG/Litva.svg";
+        String f = "test/data/filters/SVG/Neural_network_example.svg";
         IProject.FileInfo fi = loadSourceFiles(new SvgFilter(), f);
 
         checkMultiStart(fi, f);
-        skipMulti();
-        skipMulti();
-        skipMulti();
-        checkMulti("Polatsk lands", null, null, "Navahrudak lands", "Turaw-Pinsk lands", null);
-        checkMulti("Turaw-Pinsk lands", null, null, "Polatsk lands", "Pinsk", null);
-        checkMulti("Pinsk", null, null, "Turaw-Pinsk lands", "Slonim", null);
+        checkMulti("image/svg+xml", null, null, "", "<t0><t1>input</t1></t0><t2><t3>layer</t3></t2>", null);
+        checkMulti("<t0><t1>input</t1></t0><t2><t3>layer</t3></t2>", null, null, "image/svg+xml",
+                "<t0><t1>hidden</t1></t0><t2><t3>layer</t3></t2>", null);
+        checkMulti("<t0><t1>hidden</t1></t0><t2><t3>layer</t3></t2>", null, null,
+                "<t0><t1>input</t1></t0><t2><t3>layer</t3></t2>",
+                "<t0><t1>output</t1></t0><t2><t3>layer</t3></t2>", null);
     }
 }
